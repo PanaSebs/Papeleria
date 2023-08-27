bd23407f72d884683cdec5c2080e899fdec871ac
User <User@DESKTOP-V51VHNT>
2023-08-26 20:57:13 -0500

    a

---
+++ src/papeleria/Inventario.java
@@ -0,0 +1,85 @@
+package papeleria;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Inventario {
+
+    private int CAPACIDAD_MAXIMA = 100;
+    private Producto[] inventario = new Producto[CAPACIDAD_MAXIMA];
+    private int contador = 0;
+    private Scanner scanner = new Scanner(System.in);
+
+    public void agregarProducto() {
+        if (contador < CAPACIDAD_MAXIMA) {
+            System.out.print("Ingrese el codigo del producto: ");
+            String codigo = scanner.nextLine();
+            System.out.print("Ingrese el nombre del producto: ");
+            String nombre = scanner.nextLine();
+            System.out.print("Ingrese el precio del producto: ");
+            double precio = scanner.nextDouble();
+            System.out.print("Ingrese la cantidad del producto: ");
+            int cantidad = scanner.nextInt();
+            scanner.nextLine();
+
+            inventario[contador] = new Producto(codigo, nombre, precio, cantidad);
+            contador++;
+            System.out.println("Producto agregado con exito.");
+        } else {
+            System.out.println("Inventario lleno. No se pueden agregar más productos.");
+        }
+    }
+
+    public void mostrarInventario() {
+        if (contador == 0) {
+            System.out.println("No hay productos en el inventario. Agregue productos primero.");
+        } else {
+            for (int i = 0; i < contador; i++) {
+                Producto producto = inventario[i];
+                if (producto != null) {
+                    System.out.println("Codigo: " + producto.getCodigo()
+                            + ", Nombre: " + producto.getNombre()
+                            + ", Precio: " + producto.obtenerPrecio()
+                            + ", Cantidad: " + producto.getCantidad());
+                }
+            }
+        }
+    }
+    
+    public void eliminarProducto() {
+        System.out.print("Ingrese el codigo del producto a eliminar: ");
+        String codigo = scanner.nextLine();
+
+        for (int i = 0; i < contador; i++) {
+            if (inventario[i] != null && inventario[i].getCodigo().equals(codigo)) {
+                for (int j = i; j < contador - 1; j++) {
+                    inventario[j] = inventario[j + 1];
+                }
+                inventario[contador - 1] = null;
+                contador--;
+                System.out.println("Producto eliminado con exito.");
+                return;
+            }
+        }
+
+        System.out.println("Producto no encontrado en el inventario.");
+    }
+
+    public void buscarProducto() {
+        System.out.print("Ingrese el codigo del producto a buscar: ");
+        String codigo = scanner.nextLine();
+
+        for (int i = 0; i < contador; i++) {
+            if (inventario[i] != null && inventario[i].getCodigo().equals(codigo)) {
+                Producto producto = inventario[i];
+                System.out.println("Producto encontrado - Codigo: " + producto.getCodigo() +
+                        ", Nombre: " + producto.getNombre() +
+                        ", Precio: " + producto.obtenerPrecio() +
+                        ", Cantidad: " + producto.getCantidad());
+                return;
+            }
+        }
+
+        System.out.println("Producto no encontrado en el inventario.");
+    }
+}
